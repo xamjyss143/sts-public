3754fad5455aa5fa577c2043ab58e16fb3e8f5e9
XAMJYSS MAAGAD <62631365+xamjyss143@users.noreply.github.com>
2026-05-16 19:02:39 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,1 +1,18 @@
+#!/bin/sh
+set -e
 
+PUBLIC_IP="${SERVER_IP}"
+GCP_HOST="${GCP_HOST}"
+GCP_PATH="${GCP_PATH}"
+GCP_XRAY_PATH="${GCP_XRAY_PATH}"
+
+curl -X POST "https://vpnhub.cloud./xjvpn/api/server/register-gcp" \
+  -H "Content-Type: application/json" \
+  -d "{
+    \"ip\": \"$PUBLIC_IP\",
+    \"gcp_host\": \"$GCP_HOST\",
+    \"gcp_path\": \"$GCP_PATH\",
+    \"gcp_xray_path\": \"$GCP_XRAY_PATH\"
+  }" || true
+
+exec nginx -g 'daemon off;'
