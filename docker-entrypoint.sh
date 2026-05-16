6279fcd1b010bc99156b7a16d66d67a3552179a8
XAMJYSS MAAGAD <62631365+xamjyss143@users.noreply.github.com>
2026-05-16 21:45:30 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,35 @@
 #!/bin/sh
 set -e
 
-PUBLIC_IP="${SERVER_IP}"
-GCP_HOST="${GCP_HOST}"
-GCP_PATH="${GCP_PATH}"
-GCP_XRAY_PATH="${GCP_XRAY_PATH}"
+NGINX_CONF="/etc/nginx/nginx.conf"
+GCP_HOST="${GCP_HOST:sts-public-771699844894.us-central1.run.app}"
+API_URL="https://vpnhub.cloud/xjvpn/api/server/register-gcp"
 
-curl -X POST "https://vpnhub.cloud/xjvpn/api/server/register-gcp" \
-  -H "Content-Type: application/json" \
-  -d "{
-    \"ip\": \"$PUBLIC_IP\",
-    \"gcp_host\": \"$GCP_HOST\",
-    \"gcp_path\": \"$GCP_PATH\",
-    \"gcp_xray_path\": \"$GCP_XRAY_PATH\"
-  }" || true
+awk '
+/location \/[a-z0-9]+ \{/ {
+    path=$2
+}
+/proxy_pass http:\/\/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:700;/ {
+    gsub(";", "", $2)
+    gsub("http://", "", $2)
+    split($2, parts, ":")
+    ip=parts[1]
+    print path "|" ip
+}
+' "$NGINX_CONF" | while IFS="|" read -r gcp_path ip; do
+    code=$(echo "$gcp_path" | sed 's#^/##')
+    gcp_xray_path="/vmess_${code}"
+
+    echo "Registering $ip -> $gcp_path -> $gcp_xray_path"
+
+    curl -X POST "$API_URL" \
+      -H "Content-Type: application/json" \
+      -d "{
+        \"ip\": \"$ip\",
+        \"gcp_host\": \"$GCP_HOST\",
+        \"gcp_path\": \"$gcp_path\",
+        \"gcp_xray_path\": \"$gcp_xray_path\"
+      }"
+done
 
 exec nginx -g 'daemon off;'
